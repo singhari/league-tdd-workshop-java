537dbfde47b6ec7b916224baf573b422ceaf184b
singhari <110957426+singhari@users.noreply.github.com>
2024-06-19 01:58:27 +0000

    valc

--- src/main/java/leaguetdd/Calculator.java
+++ src/main/java/leaguetdd/Calculator.java
@@ -5,20 +5,35 @@ public class Calculator {
     // Main method for running the application
     public static void main(String[] args) {
         // Example usage of the methods: Uncomment after the methods are created
-        // System.out.println("Addition of 2 and 3: " + add(2, 3));
-        // System.out.println("Subtraction of 3 from 2: " + subtract(2, 3));
-        // System.out.println("Factorial of 5: " + factorial(5));
+        System.out.println("Addition of 2 and 3: " + add(2, 3));
+        System.out.println("Subtraction of 3 from 2: " + subtract(2, 3));
+        System.out.println("Factorial of 5: " + factorial(5));
     }
 
     // For all 3 methods you will create they should be pulic and static and return an integer
     
     // Create a Method to add two integers called add
-
+    public static int add(int a, int b){
+        return (a+b);
+    }
 
     // Create a Method to subtract the second integer from the first integer called subtract
-
+    public static int subtract(int a, int b){
+        return (a-b);
+    }
 
     // Create a Method to calculate the factorial of a non-negative integer called factorial
-
+    public static int factorial(int b){
+        int c = 1;
+        if(b >= 0){
+         for(int i= 1; i < b+1; i++){
+            c *= i;
+         }        
+        }
+        else{
+            c =-1;
+        }
+        return c;
+    }
 
 }
--- src/test/java/leaguetdd/TestCalculator.java
+++ src/test/java/leaguetdd/TestCalculator.java
@@ -1,17 +1,32 @@
 package leaguetdd;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
 public class TestCalculator {
 
     // For all 3 test methods you will create remember to add @Test before the method 
-
+    public static void main(String[] args) {
+        testAdd();
+        testSubtract();
+        testFactorial();
+    }
     // Create a Test for the add method called testAdd 
-
+    @Test
+    public static void testAdd(){
+        assertEquals(5,Calculator.add(2,3));
+    }
     // Create a Test for the subtract method called testSubtract
-
+    public static void testSubtract(){
+        assertEquals(-1,Calculator.subtract(2,3));
+    }
     // Create a Test for the factorial method called testFactorial
+    public static void testFactorial(){
+        assertEquals(120,Calculator.factorial(5));
+        assertEquals(-1, Calculator.factorial(-5));
+        assertEquals(1,Calculator.factorial(0));
 
+    }
 }
